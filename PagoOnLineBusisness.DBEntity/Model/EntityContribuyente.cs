ce32662fe57edf7d1fb5c9e3d54fdfad55f3f474
juliofaustor <julio.faustor@gmail.com>
2021-06-29 15:53:10 -0500

    Jfaustor : modificaciones nuevas entity, y conexion SQL a DB_E31B_GRUPO4

---
+++ PagoOnLineBusisness.DBEntity/Model/EntityContribuyente.cs
@@ -0,0 +1,21 @@
+﻿using PagoOnLineBusisness.DBEntity.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagoOnLineBusisness.DBEntity.Model
+{
+    public class EntityContribuyente : EntityBase
+    {
+        public string codigocont { get; set; }
+        public string apenom { get; set; }
+        public string tipodoc { get; set; }
+        public string nrodoc { get; set; }
+        public string dirdomicilio { get; set; }
+        public string telefono1 { get; set; }
+        public string telefono2 { get; set; }
+        public string telefono3 { get; set; }
+
+        public string correo { get; set; }
+    }
+}
